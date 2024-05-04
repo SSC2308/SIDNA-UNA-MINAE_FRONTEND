4094253481f7785a311c728d8349d11cd35f4b41
SSC2308 <69993222+SSC2308@users.noreply.github.com>
2024-05-04 13:17:32 -0600

    Update admin.js
    version 1.0.1
    Se agrego un comentario de prueba.

--- src/main/webapp/js/admin.js
+++ src/main/webapp/js/admin.js
@@ -5,7 +5,7 @@ class Admin {
 
     state;
 
-
+    //prueba 
     constructor() {
         this.state = {'entities': new Array(), 'entity': this.emptyEntity(), 'mode': 'A', usuarios: []};
         this.dom = this.render();
