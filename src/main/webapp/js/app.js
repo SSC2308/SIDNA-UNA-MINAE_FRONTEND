dac7bdffd4681caad2d8924f18be6dc30adad4bc
SSC2308 <69993222+SSC2308@users.noreply.github.com>
2024-05-09 17:44:39 -0600

    Update app.js
    Version 1.0.1
    • Se agrego un comentario de prueba.

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,6 +3,7 @@
 const backend = "https://sidnaminae-backend.onrender.com/backend";
 //const backend = "https://sidnaminae.azurewebsites.net/backend";
 //const backend = "http://localhost:8080/MINAE/minae";
+
 class App {
     dom;
     modal;
